fa834da5d093fd6e1e827b88b40ee930c73a0a9c
Maciej Sitko <maciejsitko@Maciejs-MBP.home>
2018-02-20 00:43:12 +0000

    Testing saga carrying on

--- sagas/sagas.test.js
+++ sagas/sagas.test.js
@@ -1,7 +1,9 @@
+// @flow
+import * as actionTypes from '../actions/actionTypes'
 import { all, call, fork, put } from 'redux-saga/effects'
 import rootSagas, { fetchAllTvShows, watchTvShows } from './'
 
-import * as theMovieDBService from '../services/theMovieDB' 
+import * as theMovieDBService from '../services/theMovieDB'
 
 describe('sagas', () => {
   it('exports a proper rootSagas object', () => {
@@ -20,9 +22,21 @@ describe('sagas', () => {
   describe('fetchAllTvShows', () => {
     it('follows a correct generator logic', () => {
       const iterable = fetchAllTvShows()
+      const theMovieDBResult = { results: [] }
 
       expect(iterable.next().value).toEqual(call(theMovieDBService.fetchMostPopular))
-      expect(iterable.next().value).toEqual()
+      expect(iterable.next({ results: [] }).value).toEqual(put({
+        type: actionTypes.TV_SHOWS_RESULTS,
+        payload: { theMovieDBResult }
+      }))
+      expect(iterable.next().done).toEqual(true)
+    })
+
+    it.ski('puts a failure action with proper payload', () => {
+      const iterable = fetchAllTvShows()
+
+      expect(iterable.next().value).toEqual(call(theMovieDBService.fetchMostPopular))
+      expect(iterable.next().done).toEqual(true)
     })
   })
 })
